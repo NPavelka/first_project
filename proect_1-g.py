a86ff54b1573494348ee6b5bcdf4348cc1198147
NPavelka <66479468+NPavelka@users.noreply.github.com>
2020-09-23 17:04:55 +0200

    Графики для проектной работы

--- proect_1-g.py
+++ proect_1-g.py
@@ -2,13 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #import matplotlib.animation as anim
-#from scipy.integrate import odeint
-
-#Константы
-#G = 6.67*10**(-11)   #Гравитационная постоянная
-#M1 = 5.974*10**24    #Масса первого гравитирующего объекта
-#M2 = 7.348*10**22    #Масса второго гравитирующего объекта
-#m = 2.7*10**10       #Масса астероида (что соответствует астероиду Апофис)
 
 def grafics(L1,E1,L2,E2):
     
@@ -20,8 +13,7 @@ def grafics(L1,E1,L2,E2):
 
     plt.plot(x1,y1,color='red', label='d=10000 км')
     plt.plot(x2,y2,color='blue', label='d=50000 км')
-    
-#    plt.subplots(figsize=(8,4))
+
     plt.xlabel('Расстояние до места подрыва')                      #Подпись оси Ох
     plt.ylabel('Мощность ядерного взрыва')                         #Подпись оси Оу
     plt.grid()
